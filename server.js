8cd05af7ba9cef271327c5121dab2196f095591d
yashp386 <yashp30386@gmail.com>
2024-12-01 00:00:55 +0530

    server error

--- server.js
+++ server.js
@@ -2,9 +2,21 @@ const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
 
-const port = 3000;
+const port=1234;
 
 const app=express();
+app.use(express.static(__dirname))
+
+mongoose.connect('mongodb://120.0.0.1:27017/students')
+const db = mongoose.connection
+db.once('open',()=>{
+    console.log("mongodb connected succesfully")
+})
+
+app.get('/',(req,res)=>{
+    // res.send('hi to server')  check to start server
+    res.sendFile(path.join(__dirname,'form.html'))
+})
 
 app.listen(port,()=>{
     console.log("server started");
--- tempserver.js
+++ tempserver.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3040
 
 app.get('/', function(req, res) {
   res.send('Hello World!')
 })
 
 app.listen(port)
+
+mongoose.connect('mongodb://127.0.0.1:27017/students', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+db.on('error', (error) => {
+    console.error("Error connecting to MongoDB:", error);
+});
+
+db.once('open', () => {
+    console.log("mongodb connected successfully");
+});
